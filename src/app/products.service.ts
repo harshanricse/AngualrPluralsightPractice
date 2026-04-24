fbaf82c93385227ca4306d492f57d567478c2a5d
harshanricse <tunuguntlaharsha@gmail.com>
2026-04-24 07:41:47 +0530

    calling products api from productsservice

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
-import {IProduct} from './product.model';
-import allProducts from './products.json'
+import { computed, Injectable } from '@angular/core';
+import { httpResource, HttpResourceRef } from '@angular/common/http';
+import { IProduct } from './product.model';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  getProducts() : IProduct[] {
-    return allProducts;
+  private resource : HttpResourceRef<IProduct[] | undefined> = httpResource(() => '/api/products');
+
+  getProducts() {
+    return computed(() => this.resource.value() ?? []);
   }
 }
