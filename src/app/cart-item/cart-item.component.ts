509911be3b5a23d3a4faebde94d0eadcd1b03a79
harshanricse <tunuguntlaharsha@gmail.com>
2026-04-24 07:13:14 +0530

    managing application state with angular service using signals

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal, input } from '@angular/core';
 import { IProduct } from '../product.model';
 import { CurrencyPipe, NgClass } from '@angular/common';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'bot-cart-item',
@@ -11,12 +12,16 @@ import { CurrencyPipe, NgClass } from '@angular/common';
 export class CartItemComponent {
   product = input.required<IProduct>();
 
+  constructor(private cartService: CartService){
+    
+  }
+
   getImageUrl(product: IProduct){
     return '/images/robot-parts/' + product.imageName;
   }
 
   removeFromCart(){
-    
+    this.cartService.removeFromCart(this.product());
   }
 
   getPriceClasses(){
